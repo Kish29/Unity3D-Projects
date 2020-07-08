600b05cc89863ff80cccac99127190277d17d86c
Kish29 <875691208@qq.com>
2020-07-08 12:21:50 +0800

    TowerDefense

---
+++ TowerDefense/Assets/Scripts/TestScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.blue;
+        //Gizmos.DrawSphere(transform.position, 1f);
+        var pos = transform.position;
+        // Gizmos.color = Color.blue;
+        // var drawPosZ = pos + new Vector3(0, pos.y, 0);
+        // var drawPosZ1 = drawPosZ;
+        // drawPosZ1.z += 3;
+        // Gizmos.DrawLine(drawPosZ, drawPosZ1);            
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < 4; i++)
+        {
+            var toward = transform.TransformPoint(i, 0, 3);
+            Gizmos.DrawLine(transform.TransformPoint(i, 0, 0), toward);
+        }
+
+        // Debug.Log(toward.ToString());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ TowerDefense/Assets/Scripts/TileObjects.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+
+public class TileObjects : MonoBehaviour
+{
+    // 静态实例化成员
+    public static TileObjects Instance = null;
+
+    // tile的碰撞层
+    public LayerMask tileLayer;
+
+    // tile的大小
+    public float tileSize = 1.0f;
+
+    // x轴方向的tile数量
+    public int xTileNum = 2;
+
+    // z轴方向的tile数量
+    public int zTileNum = 2;
+
+    // 所有的格子信息
+    public int[] tileData;
+
+    // 不再inspector窗口显示 
+    [HideInInspector] public int tileId = 0;
+
+    public bool debug = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // 重置地图数据
+    public void Reset()
+    {
+        tileData = new int[xTileNum * zTileNum];
+    }
+
+    // 获得某个坐标对应的tileData的索引
+    private int GetDataIndexFromPosition(float posX, float posZ)
+    {
+        var position = transform.position;
+        int index = (int) ((posX - position.x) / tileSize) * zTileNum +
+                    (int) ((posZ - position.z) / tileSize);
+        if (index < 0 || index >= tileData.Length)
+            return 0;
+        return index;
+    }
+
+    // 获得相应的tile数值
+    public int GetDataFromPosition(float posX, float posZ)
+    {
+        int index = GetDataIndexFromPosition(posX, posZ);
+        return tileData[index];
+    }
+
+    // 设置相应的tile数值
+    public void SetDataFromPosition(float poxX, float posZ, int val)
+    {
+        int index = GetDataIndexFromPosition(poxX, posZ);
+        tileData[index] = val;
+    }
+
+    // 在编辑模式显示帮助信息
+    private void OnDrawGizmos()
+    {
+        if (!debug)
+            return;
+        if (tileData == null)
+        {
+            Debug.Log("Please Reset Data First!");
+            return;
+        }
+
+        Vector3 pos = transform.position;
+
+        // 为格子画上辅助线
+        for (int i = 0; i < xTileNum; i++) // z轴方向的辅助线
+        {
+            Gizmos.color = Color.blue;
+            // 线长度
+            var drawLengthZ = tileSize * zTileNum;
+            Gizmos.DrawLine(transform.TransformPoint(tileSize * i, 0, 0),
+                transform.TransformPoint(tileSize * i, 0, drawLengthZ));
+            // 自身调用TransformPoint函数，在传入的参数不是new Vector3的情况下，
+            // 将自生的坐标加上传入的坐标x、y、z再转化成世界坐标返回
+            for (int j = 0; j < zTileNum; j++) // x轴方向的辅助线
+            {
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
